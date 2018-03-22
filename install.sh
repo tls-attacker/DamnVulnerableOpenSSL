d87a01f2c503f1976a6095835f429d57535a2320
Nimrod Aviram <nimrod.aviram@gmail.com>
2018-03-22 15:58:39 +0200

    Added patch for TLS-POODLE vulnerability.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ wget https://www.openssl.org/source/openssl-1.0.2l.tar.gz
 tar -xzf openssl-1.0.2l.tar.gz
 
 patch openssl-1.0.2l/ssl/s3_srvr.c s3_srvr.patch 
+patch openssl-1.0.2l/crypto/evp/e_aes_cbc_hmac_sha1.c e_aes_cbc_hmac_sha1.patch
 
 cd openssl-1.0.2l
 ./config
