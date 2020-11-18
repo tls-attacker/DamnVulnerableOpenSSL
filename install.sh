da658b234661c846ed3624c37fb38ea08fe2ec2d
Robert Merget (ic0ns) <robert.merget@rub.de>
2020-11-18 14:16:12 +0100

    added a padding oracle which is also present if aesni is not available

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ tar -xzf openssl-1.0.2l.tar.gz
 
 patch openssl-1.0.2l/ssl/s3_srvr.c s3_srvr.patch 
 patch openssl-1.0.2l/crypto/evp/e_aes_cbc_hmac_sha1.c e_aes_cbc_hmac_sha1.patch
-
+patch openssl-1.0.2l/ssl/s3_pkt.c patch_cbc.patch
 cd openssl-1.0.2l
 ./config
 make -j4
